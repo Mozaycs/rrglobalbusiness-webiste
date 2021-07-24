6ae499d0ae78b8923929afc0a51c8e81f8bdb76e
Husen Lokhandwala <coder.husen@gmail.com>
2021-07-24 15:27:38 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,14 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        contentTypes: [`Posts`],
+        queryLimit: 1000
+      }
+    },
     'gatsby-plugin-sass',
   ],
 }
