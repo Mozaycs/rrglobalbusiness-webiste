78e443c9b64ff7db038596a27661f76f762703d2
Husen Lokhandwala <coder.husen@gmail.com>
2021-07-27 21:45:27 +0530

    logo added

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import Logo from '../images/logo.png'
 
 import '../assets/scss/main.scss'
 
@@ -46,6 +47,7 @@ const Layout = ({ children, location }) => {
             ]}
           >
             <html lang="en" />
+            <link rel="icon" href="../images/logo.png" />
           </Helmet>
           {content}
         </>
