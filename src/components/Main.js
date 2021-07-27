b14cd3f848b841da871355f8d22d0f01d5c15fdc
Niral Bhalodia <nhbhalodia@gmail.com>
2021-07-27 16:56:14 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -116,6 +116,7 @@ class Main extends React.Component {
         >
           <h2 className="major">Contact</h2>
           <form name="contact" method="POST" data-netlify="true">
+            <input type="hidden" name="form-name" value="contact">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
